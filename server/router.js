054d107184e773ce3cb4c0980f7b107b7edb4d5e
AutumnStarz <143517148+AutumnStarz@users.noreply.github.com>
2025-04-01 15:57:50 -0400

    commit

---
+++ server/router.js
@@ -0,0 +1,16 @@
+const controller = require('./controllers');
+
+const router = (app) => {
+    app.get('/login', controller.Account.loginPage);
+    app.post('/login', controller.Account.login);
+
+    app.get('/signup', controller.Account.signupPage);
+    app.post('/signup', controller.Account.signup);
+
+    app.get('/logout', controller.Account.logout);
+    app.get('/maker', controller.Domo.makerPage);
+    app.get('/', controller.Account.loginPage);
+
+};
+
+module.exports = router;
